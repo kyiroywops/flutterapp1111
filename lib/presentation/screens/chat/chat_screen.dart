b4d3fa08d856123f7f59fe8bcba89d4c4d12ae55
Kylian Leroy <aleexisazul@gmail.com>
2023-08-02 17:59:53 -0400

    chat ui

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,50 @@
+import "package:flutter/material.dart";
+import "package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart";
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text(
+          "kkkk",
+        ),
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZVXUv9Dt-YSWQqgxkIQ0m1Pl_YNzWEwNWfh-tkK_oUARsxqqDLGVPZoJjveFK_nuV0SQ&usqp=CAU'),
+          ),
+        ),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 6.0),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 30,
+              itemBuilder: (context, index) {
+                return const MyMessageBubble();
+              },
+            )),
+            const Text("data"),
+            const Text('hola')
+          ],
+        ),
+      ),
+    );
+  }
+}
